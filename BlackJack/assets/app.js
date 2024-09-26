9641408cbdbc78c1cc31c87f56e92ca101aaae3c
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2024-09-26 19:26:42 +0200

    logica Juego y cartas

--- BlackJack/assets/app.js
+++ BlackJack/assets/app.js
@@ -1,48 +1,85 @@
-class Persona {
-    constructor(carta) {
-        this.carta=carta;
+class Juego {
+    constructor(carta,suma) {
+        this.carta = carta;
+        this.suma = suma;
     }
-    generaCartaAleatoria(){
-        const arrayPalos=[ "♠","♥" ,"♣","♦️"]
-        const arrayNum=["1","2","3","4","5","6","7","8","9","10","Q","K","J","A"];
+    juegaCartas() {
+        const arrayPalos = ["♠", "♥", "♣", "♦️"]
+        const arrayNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K", "J", "A"];
 
-        let paloAleatorio= arrayPalos[Math.round(Math.random()*(arrayPalos.length-1))];
-        let numAleatorio= arrayNum[Math.round(Math.random()*(arrayNum.length-1))];
+        let paloAleatorio = arrayPalos[Math.round(Math.random() * (arrayPalos.length - 1))];
+        let numAleatorio = arrayNum[Math.round(Math.random() * (arrayNum.length - 1))];
         this.carta.push(numAleatorio.concat(paloAleatorio));
-        console.log(this.carta);
-
+        this.suma=this.sumaCartas();
     }
-}
+    sumaCartas() {
+        let valorNumeral = this.carta.map((value) => value[0]);
 
-class Usuario extends Persona {
+        for (let index = 0; index < valorNumeral.length; index++) {
+            if (valorNumeral[index] == "Q" || valorNumeral[index] == "K" || valorNumeral[index] == "J") {
+                valorNumeral[index] = "10";
+            }
+        }
 
-    constructor(dinero,usuario,pass,carta) {
-        super(carta)
-        this.dinero=dinero;
-        this.usuario=usuario;
-        this.pass=pass;
+        let comodin=valorNumeral.indexOf("A");
 
-    }
-}
-class Cupier extends Persona {
-    constructor(dinero,carta) {
-        super(carta);
-        this.dinero=dinero;
+        if (comodin != -1) {
+            let as=valorNumeral[comodin];
+            valorNumeral.splice(comodin,1);
+            valorNumeral.push(as);
+        }
+
+        let valorNumerico=valorNumeral.map((value)=>value != "A"? parseInt(value) : value);
         
+        let total=valorNumerico.reduce((suma,valor)=>valor!="A" ? suma+=valor: suma+10<=21 ? suma+10 : suma+1);
+
+        return total
     }
 }
 
-class Carta {
-    constructor(palo,numero,url) {
-        this.palo=palo;
-        this.numero=numero;
-        this.url=url;
+class Usuario extends Juego {
+
+    constructor(dinero, usuario, pass, carta,suma) {
+        super(carta,suma)
+        this.dinero = dinero;
+        this.usuario = usuario;
+        this.pass = pass;
+
+    }
+}
+class Cupier extends Juego {
+    constructor(dinero, carta, Persona, Personas,suma) {
+        super(carta,suma);
+        this.dinero = dinero;
+        this.Persona = Persona;
+        this.Personas = Personas;
+    }
+}
+
+class Cartas {
+    constructor( url,cartas) {
+        this.url = url;
+        this.cartas=cartas;
+    }
+    muestraCartas(){
+        let urlCartas=this.cartas.map((value)=>value="img/"+value+".jpg"    )
+
+        this.url.push(urlCartas);
+        console.log(this.url)
     }
 
 }
 
-const jose=new Usuario(300,"Jarmoreno",1231231,[]);
+const jose = new Usuario(300, "Jarmoreno", 1231231, []);
+const cartaJose =new Cartas([],jose.carta)
 
-jose.generaCartaAleatoria();
-jose.generaCartaAleatoria();
-jose.generaCartaAleatoria();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+jose.juegaCartas();
+cartaJose.muestraCartas();
