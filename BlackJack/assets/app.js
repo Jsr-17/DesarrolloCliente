b4412a10dece54954544ee533841fed1084dcb5c
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2024-10-05 15:02:53 +0200

    Añadido boton para seguir jugando y toda la logica para que funciones

--- BlackJack/assets/app.js
+++ BlackJack/assets/app.js
@@ -8,6 +8,8 @@ class Baraja {
         this.mazo = [];
     }
     creaMazo() {
+        //resetea mazo
+        this.mazo = [];
         //Definicion de variables globales
 
 
@@ -89,8 +91,15 @@ class Juego {
             valorNumeral.splice(comodin, 1);
             valorNumeral.push(as);
         }
+        //En mi black Jack si un jugador saca doble comodin directamente suma 21
+
+        if (valorNumeral[0] == "A" && valorNumeral[1] == "A") {
+            valorNumeral[1] = 10;
+            valorNumeral[0] = 11;
+        }
+
         //Convierto cada numero distinto al comodin a número
-        let valorNumerico = valorNumeral.map((value) => value != "A" ? parseInt(value) : value);
+        let valorNumerico = valorNumeral.map((value) => value != "A" ? value * 1 : value);
 
         //Aplico la suma donde también incluyo el caso del comodín el cual según el contexto suma 11 o solo 1
         let total = valorNumerico.reduce((suma, valor) => valor != "A" ? suma += valor : suma + 11 <= 21 ? suma + 11 : suma + 1);
@@ -109,7 +118,7 @@ class Juego {
 
 class Usuario extends Juego {
 
-    constructor(dinero, usuario, pass, carta, suma, pierde, pasar,activo) {
+    constructor(dinero, usuario, pass, carta, suma, pierde, pasar, activo) {
         super(carta, suma, pierde)
         this.dinero = dinero;
         this.usuario = usuario;
@@ -130,23 +139,93 @@ class Cupier extends Juego {
         super(carta, suma);
         this.dinero = dinero;
         this.personas = personas;
-        this.turno=true
+        this.turno = true
     }
-    empiezaTurno(baraja){
-        this.turno=true
+    //Función con la cual el cupier empieza a jugar
+    empiezaTurno(baraja) {
+        //reseteo el turno
+
+        this.turno = true
+        //hace la comprobación de que nadie aun haya perdido o se haya plantado
         for (let index = 0; index < this.personas.length; index++) {
             if (!this.personas[index].pierde && !this.personas[index].pasar) {
-                return this.turno=false;
+                return this.turno = false;
             }
-            
-        }
-        if (this.turno) {
-            console.log("hola");
-            this.personas.forEach(value=>{
 
-                value.activo ? value.suma> this.suma ?  this.juegaCartas(baraja) :console.log("es menor") : console.log("No activo");
-            });
         }
+        //empieza el turno del cupier
+        if (this.turno) {
+            //Variable para guardar el numero mas alto de los jugadores
+            let masAlta = 0;
+
+            for (let index = 0; index < this.personas.length; index++) {
+                //Almaceno la suma más alta de las personas
+                this.personas[index].suma <= 21 && this.personas[index].suma > masAlta ? masAlta = this.personas[index].suma : masAlta;
+
+                masAlta == 21 ? masAlta - 2 : masAlta;
+
+                //El cupier saca cartas hasta superar esa cifra o sacar 21
+
+                while (masAlta > cupier.suma && cupier.suma <= 21) {
+                    this.juegaCartas(baraja.sacaCarta());
+                }
+
+                //Resetea el html 
+                cupierZona.innerHTML = '<h1>Cupier  Suma Total: <span id="sumCupier"></span> </h1>'
+                let suma = document.getElementById("sumCupier");
+                suma.innerText = cupier.suma;
+            }
+            //Elimino la primera carta para que no se repita en el html
+            cartasCupier.url.shift();
+            //Muestra las cartas del cupier
+            cartasCupier.muestraCartas();
+            //Elimina el boton de pasar
+            jugadorPlantarse.forEach((value) => value.style.display = "none");
+            jugadorPedirCarta.forEach((element) => element.style.display = "none");
+
+
+            //filtro los que han ganado 
+            let ganadores = this.personas.filter((value) => value.suma > cupier.suma && value.suma <= 21 && value.activo || cupier.suma > 21 && value.suma <= 21 && value.activo);
+            let empates = this.personas.filter((value) => value.suma == cupier.suma && value.suma <= 21 && value.activo);
+
+            //Recupero sus nombres
+            let nombreGanadores = ganadores.map((value) => value.usuario);
+            const nombreEmpate = empates.map((value) => value.usuario);
+
+            //Creo las cabeceras
+            const h2Ganador = document.createElement("h2");
+            const h2Empate = document.createElement("h2");
+
+            // En el caso de que haya ganadores lo plasmo en el html
+            if (ganadores.length >= 1) {
+                h2Ganador.innerText = "Victoria de:  ";
+
+                for (let index = 0; index < nombreGanadores.length; index++) {
+                    h2Ganador.innerText += " " + nombreGanadores[index];
+                }
+                h2Ganador.style.border = "solid 3px black";
+                h2Ganador.style.marginTop = "4px";
+
+                cupierZona.append(h2Ganador);
+            }
+
+            // En el caso de que haya empates lo plasmo en el html
+
+            if (empates >= 1) {
+                h2Empate.innerText = "Empate de: ";
+
+                for (let index = 0; index < nombreEmpate.length; index++) {
+                    h2Empate.innerText += " , " + nombreEmpate[index];
+                }
+                h2Empate.style.border = "solid 3px black";
+                h2Empate.style.marginTop = "4px";
+                cupierZona.append(h2Empate);
+            }
+
+
+
+        }
+    jugarDeNuevo.style.display="block";
     }
 }
 
@@ -203,7 +282,7 @@ class Cartas {
 //Método encargado de la modificación de la carga en pantalla de las cartas y de muestra de información en pantalla
 
 //Los parámetros son las partes del objeto importantes del funcionamiento de esta lógica
-const cojeCartaDom = (jugador, cartasJugador, sumaJugador, zonaJugador, e,cupier) => {
+const cojeCartaDom = (jugador, cartasJugador, sumaJugador, zonaJugador, e, cupier) => {
     if (jugador.pasar) {
         return;
     }
@@ -218,32 +297,144 @@ const cojeCartaDom = (jugador, cartasJugador, sumaJugador, zonaJugador, e,cupier
         zonaJugador.style.display = "none";
         sumaJugador.style.fontSize = "20px";
         sumaJugador.innerHTML = "Te has pasado tu total es de: " + jugador.suma;
-        cupier.empiezaTurno(baraja.sacaCarta());
+        cupier.empiezaTurno(baraja);
         return;
     }
     //En caso contrario Seguimos con el juego
     sumaJugador.innerHTML = jugador.suma;
-    cupier.empiezaTurno(baraja.sacaCarta());
+    cupier.empiezaTurno(baraja);
+}
+
+
+const reiniciaJuego = () => {
+    baraja.creaMazo();
+
+    jugadores.map((value) => value.carta = []);
+    cartas.map((value) => {
+        value.cartas = [];
+        value.url = [];
+    }
+    );
+    cupier.carta = [];
+    cupier.suma = 0;
+    cartasCupier.url = [];
+    cartasCupier.cartas = cupier.carta
+
+    const divPlantarse = document.querySelectorAll(".plantarse");
+    const img = document.querySelectorAll('img');
+    const h2 = document.querySelectorAll('h2');
+    
+
+    h2.forEach((element) => {
+        element.remove()
+    });
+
+
+    img.forEach((element) => {
+        element.remove();
+    });
+
+    divPlantarse.forEach((element) => {
+        element.remove();
+    })
+
+    for (let index = 0; index < nJugadores; index++) {
+        let contenedor = jugadoresZon[index].childNodes.item(3);
+
+        contenedor.childNodes[1].style.display = "block";
+        contenedor.childNodes[3].style.display = "block";
+
+
+        jugadores[index].juegaCartas(baraja.sacaCarta());
+        jugadores[index].juegaCartas(baraja.sacaCarta());
+
+        jugadores[index].activo=true;
+        jugadores[index].pasar=false;
+        jugadores[index].pierde=false;
+
+        jugadoresDin[index].innerHTML = jugadores[index].dinero;
+        jugadoresSum[index].innerHTML = jugadores[index].suma;
+
+        cartas[index].cartas = jugadores[index].carta;
+
+        cartas[index].muestraCartas();
+    }
+    cupier.juegaCartas(baraja.sacaCarta());
+    cupierZona.innerHTML = '<h1>Cupier  Suma Total: <span id="sumCupier"></span> </h1>'
+    
+    let suma = document.getElementById("sumCupier");
+    suma.innerText = cupier.suma;
+    
+    cartasCupier.muestraCartas();
+    let imgn = document.createElement('img');
+    imgn.src = "./assets/img/reverso-gris.png";
+    cupierZona.appendChild(imgn);
+
+    cupier.juegaCartas(baraja.sacaCarta());
+
+
 }
 
 
 
 
-const plantarse = (jugador,elemento,cupier) => {
-       if (!jugador.pasar && !jugador.pierde ) {
-        jugador.pasar=true;
+const iniciaJuego = () => {
+
+    menuJugadores.style.display = "none";
+    modal.style.display = "none";
+
+    nJugadores = obtnJugadores.value;
+
+    cupier.juegaCartas(baraja.sacaCarta());
+    cartasCupier.muestraCartas();
+
+
+    let img = document.createElement('img');
+    img.src = "./assets/img/reverso-gris.png";
+    cupierZona.appendChild(img);
+    cupierSum.innerHTML = cupier.suma;
+    cupier.juegaCartas(baraja.sacaCarta());
+
+
+    //Itera por cada jugador y comprueba segun el índice si es un jugador activo o no 
+    jugadores.forEach((jugador, index) => {
+        jugador.pasar = (index + 1) > nJugadores ? true : false;
+        jugador.activo = jugador.pasar ? false : true;
+    });
+
+    //Inicializa los datos de los jugadores activos 
+
+    for (let index = 0; index < nJugadores; index++) {
+
+        jugadores[index].juegaCartas(baraja.sacaCarta());
+        jugadores[index].juegaCartas(baraja.sacaCarta());
+
+        jugadoresDin[index].innerHTML = jugadores[index].dinero;
+        jugadoresSum[index].innerHTML = jugadores[index].suma;
+
+        cartas[index].muestraCartas();
+
+    }
+}
+
+//Función con la que cada jugador se planta y no pide mas cartas, y lo plasma en el html 
+const plantarse = (jugador, elemento, cupier) => {
+    if (!jugador.pasar && !jugador.pierde) {
+        jugador.pasar = true;
 
         let div = document.createElement('div');
-        div.innerText="SE HA PLANTADO";
-        div.style.marginLeft="3px";
-        div.style.padding="3px";
-        div.style.border="solid 3px black";
-        div.style.backgroundColor="white"
-        div.style.textAlign="center";
-        
-        elemento.parentElement.appendChild(div);    
-       } 
-       cupier.empiezaTurno(baraja.sacaCarta());
+        div.innerText = "SE HA PLANTADO";
+        div.style.marginLeft = "3px";
+        div.style.padding = "3px";
+        div.style.border = "solid 3px black";
+        div.style.backgroundColor = "white"
+        div.style.textAlign = "center";
+        div.classList.add("plantarse");
+
+        elemento.parentElement.appendChild(div);
+    }
+    cupier.empiezaTurno(baraja);
+
 }
 
 //------------------------------------------------------------Eventos y manejo del DOM-----------------------------------------------------------
@@ -258,6 +449,9 @@ const menuInicio = document.querySelector(".menuInicio");
 const menuJugadores = document.querySelector(".menuJugadores");
 const obtnJugadores = document.getElementById("nJugadores");
 const iniciarJuego = document.getElementById("iniciarJuego");
+const jugarDeNuevo=document.getElementById("juegoNuevo");
+
+
 
 //Zonas de jugador
 const cupierZona = document.getElementById("cupier");
@@ -323,8 +517,11 @@ const cartasjugador4 = new Cartas([], jugador4.carta, jugador4Zona);
 //Propiedades agrupadas
 const jugadores = [jugador1, jugador2, jugador3, jugador4];
 const cartas = [cartasjugador1, cartasjugador2, cartasjugador3, cartasjugador4];
+const jugadoresZon = [jugador1Zona, jugador2Zona, jugador3Zona, jugador4Zona]
 const jugadoresDin = [jugador1Din, jugador2Din, jugador3Din, jugador4Din];
 const jugadoresSum = [jugador1Sum, jugador2Sum, jugador3Sum, jugador4Sum];
+const jugadorPlantarse = [j1Pasar, j2Pasar, j3Pasar, j4Pasar];
+const jugadorPedirCarta = [j1PideCarta, j2PideCarta, j3PideCarta, j4PideCarta]
 
 
 
@@ -340,6 +537,8 @@ const cartasCupier = new Cartas([], cupier.carta, cupierZona);
 //------------------------------------Variables------------------------------------------
 
 let nJugadores;
+
+
 
 
 
@@ -370,60 +569,29 @@ invitado.addEventListener("click", () => {
 
 
 //Esconde el menú  de nº Jugadores y muestra la partida con el tablero lleno;
-iniciarJuego.addEventListener("click", () => {
-
-    menuJugadores.style.display = "none";
-    modal.style.display = "none";
-
-    nJugadores = obtnJugadores.value;
-
-    cupier.juegaCartas(baraja.sacaCarta());
-    cartasCupier.muestraCartas();
+iniciarJuego.addEventListener("click", () => iniciaJuego());
 
 
-    let img = document.createElement('img');
-    img.src = "./assets/img/reverso-gris.png";
-    cupierZona.appendChild(img);
-    cupierSum.innerHTML = cupier.suma;
-
-    //Itera por cada jugador y comprueba segun el índice si es un jugador activo o no 
-    jugadores.forEach((jugador, index) => {
-        jugador.pasar = (index + 1) > nJugadores ? true : false;
-        jugador.activo= jugador.pasar ? false : true;
-    });
-
-    //Inicializa los datos de los jugadores activos 
-
-    for (let index = 0; index < nJugadores; index++) {
-
-        jugadores[index].juegaCartas(baraja.sacaCarta());
-        jugadores[index].juegaCartas(baraja.sacaCarta());
-
-        jugadoresDin[index].innerHTML = jugadores[index].dinero;
-        jugadoresSum[index].innerHTML = jugadores[index].suma;
-
-        cartas[index].muestraCartas();
-
-    }
-});
+jugarDeNuevo.addEventListener("click",()=>reiniciaJuego())
 
 
 
 
 //Eventos generales de la pedida de las  cartas 
-j1PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador1, cartasjugador1, jugador1Sum, jugador1Zona, e,cupier));
+j1PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador1, cartasjugador1, jugador1Sum, jugador1Zona, e, cupier));
 
-j2PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador2, cartasjugador2, jugador2Sum, jugador2Zona, e,cupier));
+j2PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador2, cartasjugador2, jugador2Sum, jugador2Zona, e, cupier));
 
-j3PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador3, cartasjugador3, jugador3Sum, jugador3Zona, e,cupier));
+j3PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador3, cartasjugador3, jugador3Sum, jugador3Zona, e, cupier));
 
-j4PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador4, cartasjugador4, jugador4Sum, jugador4Zona, e,cupier));
+j4PideCarta.addEventListener("click", (e) => cojeCartaDom(jugador4, cartasjugador4, jugador4Sum, jugador4Zona, e, cupier));
 
 
 
 //Evento de plantarse y no pedir más cartas
 
-j1Pasar.addEventListener("click", ()=>plantarse(jugador1,j1Pasar,cupier));
-j2Pasar.addEventListener("click", ()=>plantarse(jugador2,j2Pasar,cupier));
-j3Pasar.addEventListener("click", ()=>plantarse(jugador3,j3Pasar,cupier));
-j4Pasar.addEventListener("click", ()=>plantarse(jugador4,j4Pasar,cupier));
+j1Pasar.addEventListener("click", () => plantarse(jugador1, j1Pasar, cupier));
+j2Pasar.addEventListener("click", () => plantarse(jugador2, j2Pasar, cupier));
+j3Pasar.addEventListener("click", () => plantarse(jugador3, j3Pasar, cupier));
+j4Pasar.addEventListener("click", () => plantarse(jugador4, j4Pasar, cupier));
+
