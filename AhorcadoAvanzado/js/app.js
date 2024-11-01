0b0e2ced314abf593ae47165919c74937cbd24d3
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2024-11-01 16:02:34 +0100

    echo

--- AhorcadoAvanzado/js/app.js
+++ AhorcadoAvanzado/js/app.js
@@ -4,11 +4,6 @@ const generaNumAleatorio = (num) => {
 };
 
 //Elementos seleccionados del dom
-
-const contenedorIntentos = document.querySelector("#contenedorIntentos");
-const contenedorPalabra = document.querySelector("#contenedorPalabra");
-const canvas = document.querySelector("#ahorcadoCanvas");
-const btnActivaEvento = document.querySelector("#btnActivaPalabra");
 const main = document.querySelector("#main");
 const iniciaJuego = document.querySelector("#iniciaJuego");
 const dificultad = document.querySelector("#dificultad");
@@ -18,10 +13,58 @@ const dificultad = document.querySelector("#dificultad");
 class Ahorcado {
   palabra = "";
   intentos = "";
-  constructor(intentos, palabra) {
-    this.intentos = intentos;
-    this.palabra = palabra;
+  constructor() {
+    this.creaJuego().then((res) => {
+      this.palabra = res.palabra;
+      this.intentos = res.intento;
+    });
   }
+
+  creaJuego = async () => {
+    let propDificultad;
+    let propPalabra;
+    const nEleccion = dificultad.selectedIndex;
+    const valor = dificultad[nEleccion].value;
+    let faciles = [];
+    let dificiles = [];
+    let medio = [];
+    const res = await fetch("http://localhost:3000/palabras");
+    const data = await res.json();
+    const dataSinEstructurar = data.map(({ palabra }) => palabra);
+    dataSinEstructurar.map((data) => {
+      if (data.length <= 5) {
+        faciles.push(data);
+      } else if (data.length <= 10) {
+        medio.push(data);
+      } else {
+        dificiles.push(data);
+      }
+    });
+    switch (valor) {
+      case "facil":
+        propDificultad = 6;
+        propPalabra = faciles[generaNumAleatorio(faciles.length)];
+        break;
+      case "medio":
+        propDificultad = 5;
+        propPalabra = medio[generaNumAleatorio(medio.length)];
+
+        break;
+      case "dificil":
+        propDificultad = 3;
+        propPalabra = dificiles[generaNumAleatorio(dificiles.length)];
+
+        break;
+
+      default:
+        break;
+    }
+    const datos = {
+      intento: propDificultad,
+      palabra: propPalabra,
+    };
+    return datos;
+  };
 }
 
 //+++++++++++++++++++++++++++++++++++Logica Juego ++++++++++++++++++++++
@@ -48,48 +91,13 @@ iniciaJuego.addEventListener("click", () => {
   </button>
 </div>
 </div>`;
-  const nEleccion = dificultad.selectedIndex;
-  const valor = dificultad[nEleccion].value;
-  let propDificultad = 0;
-  let propPalabra = "";
+  const juego = new Ahorcado();
 
-  const creaJuego = async () => {
-    let faciles = [];
-    let dificiles = [];
-    let medio = [];
-    const res = await fetch("http://localhost:3000/palabras");
-    const data = await res.json();
-    const dataSinEstructurar = data.map(({ palabra }) => palabra);
-    dataSinEstructurar.map((data) => {
-      if (data.length <= 5) {
-        faciles.push(data);
-      } else if (data.length <= 10) {
-        medio.push(data);
-      } else {
-        dificiles.push(data);
-      }
-    });
-    switch (valor) {
-      case "facil":
-        propDificultad = 10;
-        propPalabra = faciles[generaNumAleatorio(faciles.length)];
-        break;
-      case "medio":
-        propDificultad = 5;
-        propPalabra = medio[generaNumAleatorio(medio.length)];
+  //Elementos seleccionados del dom
 
-        break;
-      case "dificil":
-        propDificultad = 5;
-        propPalabra = dificiles[generaNumAleatorio(dificiles.length)];
-
-        break;
-
-      default:
-        break;
-    }
-
-    const juego = new Ahorcado(propDificultad, propPalabra);
-  };
-  creaJuego();
+  const contenedorIntentos = document.querySelector("#contenedorIntentos");
+  const contenedorPalabra = document.querySelector("#contenedorPalabra");
+  const canvas = document.querySelector("#ahorcadoCanvas");
+  const btnActivaEvento = document.querySelector("#btnActivaPalabra");
+  btnActivaEvento.addEventListener("click", () => console.log(juego.palabra));
 });
